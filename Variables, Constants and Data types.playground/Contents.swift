5bc3c92a2b0eeda2b3567722e2a814633d804f72
Al <afsm1995@live.com>
2017-12-23 15:10:56 -0700

    Playground

---
+++ Variables, Constants and Data types.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAge: Int = 32
+
+myAge = 33
+
+let myName: String = "Angela" // let constant
+
+// Variable like a open box
+// Constants like a closed box
+// Constants take up less space
+
+let myAgeInTenYears = myAge + 10
+
+let mySurName = "Yu"
+
+let myFullName = myName + " Yu"
+
+let myFullName2 = "\(myName) \(mySurName)"
+
+let myDetails = "\(myName), \(myAge)" // Setting value set to string to save time on typing but can be confusing when looking the code over
+
+let wholeNumbers: Int = 12
+
+let text: String = "abc"
+
+let booleans: Bool = true
+
+let floatingPointNumber: Float = 1.3
+
+let double: Double = 3.14159265359
