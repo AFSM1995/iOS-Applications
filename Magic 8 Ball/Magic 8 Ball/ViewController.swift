a5123e7d42176bf4c9c49f16b95227329c4d278b
Al <afsm1995@live.com>
2017-12-23 15:07:01 -0700

    Complete app

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,35 @@
+//  ViewController.swift
+//  Magic 8 Ball
+
+//  Created by Alvaro Santillan on 12/9/17.
+//  Copyright © 2017 Alvaro Santillan. All rights reserved.
+
+import UIKit
+
+
+class ViewController: UIViewController
+{
+    override func mutableArrayValue(forKey key: String) -> NSMutableArray
+    {
+        (ball1, ball2, ball3, ball4, ball5)
+    }
+    
+    int arc4random_uniform(4)
+    
+    @IBOutlet weak var imageView: UIImageView!
+    // Conecting 8 Ball image to code
+    
+    override func viewDidLoad( )
+    {
+        super.viewDidLoad( )
+        
+    }
+
+    override func didReceiveMemoryWarning( )
+    {
+        super.didReceiveMemoryWarning( )
+        
+    }
+    
+    
+}
