41e70ba1284bb18b3b82fdd26bbbda1e2a35fb7f
Alvaro Santillan <afsm1995@live.com>
2020-12-27 15:41:57 -0700

    Flash Chat: App Now Saves, Loads And Orders Database Data.

--- Flash Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,11 +14,8 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
-    var message: [Message] = [
-        Message(sender: "1@2.com", body: "Hey?"),
-        Message(sender: "a@b.com", body: "Yo"),
-        Message(sender: "1@2.com", body: ":|")
-    ]
+    let db = Firestore.firestore()
+    var messageArray: [Message] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,9 +27,56 @@ class ChatViewController: UIViewController {
         
         // Register custom cell
         tableView.register(UINib(nibName: Constants.cellNibName, bundle: nil), forCellReuseIdentifier: Constants.cellIdentifier)
+        // Retrive messages from database.
+        loadMessages()
+    }
+    
+    func loadMessages() {
+        db.collection(Constants.FStore.collectionName)
+            // Sorts messages by the dateField variable that we created and is assigned to a message on its conseption.
+            .order(by: Constants.FStore.dateField)
+            // addSnapshotListener adds a lisiner and reloads all the data on change.
+            .addSnapshotListener { (querySnapshot, error) in
+            
+            // The privious presented data sould be deleted to prevent duplicate messages from showing.
+            self.messageArray = []
+            
+            if let e = error {
+                print(e)
+            } else {
+                if let snapShotDocuments = querySnapshot?.documents {
+                    for doc in snapShotDocuments {
+                        let data = doc.data()
+                        if let senderBody = data[Constants.FStore.senderField] as? String, let messageBody = data[Constants.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: senderBody, body: messageBody)
+                            self.messageArray.append(newMessage)
+                            
+                            // Good pactice, get hold of main queue since grabbing documents is happening in the background.
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(Constants.FStore.collectionName).addDocument(data: [
+                Constants.FStore.dateField: Date().timeIntervalSince1970,
+                Constants.FStore.senderField: messageSender,
+                Constants.FStore.bodyField: messageBody
+            ]) { (error) in
+                if let e = error {
+                    print(e)
+                } else {
+                    print("Successfully saved data.")
+//                    self.tableView.reloadData()
+                }
+            }
+        }
     }
     
     @IBAction func logOutButtonPressed(_ sender: UIBarButtonItem) {
@@ -48,14 +92,14 @@ class ChatViewController: UIViewController {
 extension ChatViewController: UITableViewDataSource {
     // Determin how many rows to make.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return message.count
+        return messageArray.count
     }
     
     // Populate the rows with information.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Cast as custom "MessageCell" we created.
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath) as! MessageCell
-        cell.message.text = message[indexPath.row].body
+        cell.message.text = messageArray[indexPath.row].body
         return cell
     }
 }
